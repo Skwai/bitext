f5c34d4b35f34a41df0046684fc1bf703fd65bcd
Cameron <cameron@biteable.com>
2017-11-20 09:46:07 +1100

    added notify class

---
+++ functions/notify/Notify.js
@@ -0,0 +1,142 @@
+const fetch = require('node-fetch')
+const Twilio = require('twilio')
+
+const COINDESK_API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+const LT = 'LT'
+const GT = 'GT'
+
+class Notify {
+  constructor ({
+    db,
+    from,
+    twilioAccountSID,
+    twilioAuthToken
+  }) {
+    Object.assign(this, {
+      db,
+      from,
+      twilioAccountSID,
+      twilioAuthToken
+    })
+
+    this.twilio = Notify.createTwilioClient(twilioAccountSID, twilioAuthToken)
+  }
+
+  /**
+   * Create a new Twilio instance
+   * @param {String} twilioAccountSID
+   * @param {String} twilioAuthToken
+   * @return {Twilio}
+   */
+  static createTwilioClient (twilioAccountSID, twilioAuthToken) {
+    return new Twilio(twilioAccountSID, twilioAuthToken)
+  }
+
+  /**
+   * Get the current Bitcoin price
+   * @return {Promise.<Number>}
+   */
+  getPrice () {
+    return fetch(COINDESK_API_URL)
+      .then((res) => res.json())
+      .then((data) => data.bpi.USD.rate_float)
+  }
+
+  /**
+   * Get users who want to be notified of the price
+   * @param {Number} price
+   * @return {Promise.<Array>}
+   */
+  getUsers (price) {
+    return Promise.all([
+      this.getHighUsers(price),
+      this.getLowUsers(price)
+    ]).then(([high, low]) => [].concat(high.docs || [], low.docs || []))
+  }
+
+  /**
+   * Get users who want to be notified of a high price
+   * @param {Number} price
+   * @return {Promise}
+   */
+  getHighUsers (price) {
+    const collection = this.db.collection('users')
+    return collection.where('notified', '==', null)
+      .where('dir', '==', GT)
+      .where('price', '<=', price)
+      .get()
+  }
+
+  /**
+   * Get users who want to be notified of a low price
+   * @param {Number} price
+   * @return {Promise}
+   */
+  getLowUsers (price) {
+    const collection = this.db.collection('users')
+    return collection.where('notified', '==', null)
+      .where('dir', '==', LT)
+      .where('price', '>=', price)
+      .get()
+  }
+
+  /**
+   * Get a user's phone number
+   * @param {DocumentSnapshot} user
+   * @return {Array}
+   */
+  getUserPhoneNumber (userData) {
+    const { phoneCountryCode, phoneNumber } = userData
+    return [phoneCountryCode, phoneNumber].join('')
+  }
+
+  /**
+   *
+   * @param {DocumentSnapshot} user
+   * @param {String} message
+   * @return {Promise.<Object>}
+   */
+  sendUserMessage ({ from, user, message }) {
+    const userData = user.data()
+    const to = this.getUserPhoneNumber(userData)
+    return this.sendMessage({ from, to, message })
+      .then(() => this.setUserNotified(user))
+      .catch((err) => console.error(err))
+  }
+
+  /**
+   * Send an SMS
+   * @param {String} to Phone number to send message to
+   * @param {String} body The message to send
+   * @return {Promise.<Object>}
+   */
+  sendMessage ({ from, to, body }) {
+    return this.twilio.messages.create({
+      to,
+      body,
+      from
+    })
+  }
+
+  /**
+   * Format price
+   * @param {Number} price To format
+   * @return {String}
+   */
+  formatPrice (price) {
+    return `$${price.toFixed(2).toLocaleString()}`
+  }
+
+  /**
+   * Set notified on the user
+   * @param {DocumentSnapshot} user
+   * @return {Promise.<Object>}
+   */
+  setUserNotified (user) {
+    return user.ref.update({
+      notified: new Date()
+    })
+  }
+}
+
+module.exports = Notify
