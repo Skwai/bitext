da9285c32a023b8b28e80bd718ddbbfdaad8a0db
Cameron <cameron@biteable.com>
2017-12-20 21:32:34 +1100

    fixed function

--- functions/notify/index.js
+++ functions/notify/index.js
@@ -25,17 +25,23 @@ module.exports = functions.https.onRequest((req, res) => {
       console.info(`Fetched price: ${formattedPrice}`)
       message = `Hi. Bitcoin is now at ${formattedPrice} USD. This is a one-time alert`
       return notify.getUsers(price)
+        .then((users) => {
+          if (!users.length) {
+            console.log('No users to notify')
+            return Promise.resolve()
+          }
+
+          console.info(`Messaging users: ${users.length}`)
+          return Promise.all(users.map((user) => notify.sendUserMessage({
+            from,
+            user,
+            message
+          })))
+        })
+        .catch(() => {
+          console.error('Could not retrieve users')
+        })
     })
     .catch((err) => console.error(`Could not retrieve price: ${err.message}`))
-    .then((users) => {
-      console.info(`Messaging users: ${users.length}`)
-      if (users.length) {
-        return Promise.all(users.map((user) => notify.sendUserMessage({
-          from,
-          user,
-          message
-        })))
-      }
-    })
     .then(() => res.sendStatus(204).end())
 })
