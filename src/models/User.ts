f21eb5b4905490a8f3e2abb7d2257d323209c497
Cameron <cameron@biteable.com>
2018-02-02 11:36:28 +1100

    tslint changes

---
+++ src/models/User.ts
@@ -0,0 +1,8 @@
+export default class User {
+  public created: Date = new Date()
+  public notified: Date | null = null
+  public price: number | null = null
+  public dir: 'LT' | 'GT' = 'GT'
+  public phoneCountryCode: string | null = '+1'
+  public phoneNumber: string | null = null
+}
