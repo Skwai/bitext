a87690826f343d81b9e2ee5d03886f7af207e1a7
Cameron <cameron@biteable.com>
2018-03-31 23:42:14 +1100

    function changes to detect threshold change

--- functions/src/notify/index.ts
+++ functions/src/notify/index.ts
@@ -15,37 +15,46 @@ export default functions.https.onRequest(async (req, res) => {
   })
 
   try {
+    const prevPrice = await notify.getPreviousPrice()
+
     // Get the current bitcoin price
-    let price
+    let currPrice
     try {
-      price = await notify.getPrice()
+      currPrice = await notify.getPrice()
+      await notify.saveCurrentPrice(currPrice)
     } catch (err) {
       throw Error(`Could not retrieve price: ${err.message}`)
     }
 
-    const formattedPrice = Notify.formatPrice(price)
+    // If we don't have a previous price then end here
+    if (prevPrice === null) {
+      throw Error(`There was no previous price in the database`)
+    }
+
+    const formattedPrice = Notify.formatPrice(currPrice)
     console.info(`Fetched price: ${formattedPrice}`)
 
     // Get the users to notify
     let users
     try {
-      users = await notify.getUsers(price)
+      users = await notify.getUsersToNotify(currPrice, prevPrice)
     } catch (err) {
-      throw Error('Could not retrieve users from Firestore')
+      throw Error(`Could not retrieve users from Firestore: ${err.message}`)
     }
 
+    // Check if we have any users to notify
     if (!users.length) {
       console.info('No users to notify')
       return
     }
 
-    // Message users
+    // Send SMS to all of the users
     const message = `Hi. Bitcoin is now at ${formattedPrice} USD. This is a one-time alert`
     console.info(`Messaging users: ${users.length}`)
     try {
       await notify.sendUsersNotification({ from, users, message })
     } catch (err) {
-      throw Error('Failed to notify users')
+      throw Error(`Failed to notify users: ${err.message}`)
     }
     console.info('Messaging complete')
   } catch (err) {
