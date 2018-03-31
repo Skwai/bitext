b196624cb5de1d1a04b7aba3e803523446fc4346
Cameron <cameron@biteable.com>
2018-04-01 00:03:07 +1100

    Remove dir from e2e test

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -7,7 +7,6 @@ describe('App', () => {
     cy.get('form')
     cy.get('#AppForm__CountryCode').select('+61')
     cy.get('#AppForm__PhoneNumber').type('412345678')
-    cy.get('#AppForm__Dir').select('GT')
     cy.get('#AppForm__Price').type(5000000)
     cy.get('button').click()
   })
